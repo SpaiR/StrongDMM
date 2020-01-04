3c03a48d4a2327a5160f3aaeb2a155e41541425a
SpaiR <klimbetoo@gmail.com>
2020-01-04 22:15:36 +0300

    [imgui] fix action controller on environment/map switching

--- src/main/kotlin/strongdmm/controller/action/ActionController.kt
+++ src/main/kotlin/strongdmm/controller/action/ActionController.kt
@@ -16,43 +16,12 @@ class ActionController : EventConsumer, EventSender {
         consumeEvent(Event.ActionController.AddAction::class.java, ::handleAddAction)
         consumeEvent(Event.ActionController.UndoAction::class.java, ::handleUndoAction)
         consumeEvent(Event.ActionController.RedoAction::class.java, ::handleRedoAction)
+        consumeEvent(Event.Global.ResetEnvironment::class.java, ::handleResetEnvironment)
+        consumeEvent(Event.Global.SwitchMap::class.java, ::handleSwitchMap)
+        consumeEvent(Event.Global.CloseMap::class.java, ::handleCloseMap)
     }
 
-    private fun handleAddAction(event: Event<Undoable, Unit>) {
-        getCurrentActionStack()?.let { (undo, redo) ->
-            redo.clear()
-            undo.push(event.body)
-            updateActionBalance(true)
-        }
-    }
-
-    private fun handleUndoAction() {
-        getCurrentActionStack()?.let { (undo, redo) ->
-            redo.push(undo.pop().doAction())
-            updateActionBalance(false)
-            sendEvent(Event.Global.RefreshFrame())
-        }
-    }
-
-    private fun handleRedoAction() {
-        getCurrentActionStack()?.let { (undo, redo) ->
-            undo.push(redo.pop().doAction())
-            updateActionBalance(true)
-            sendEvent(Event.Global.RefreshFrame())
-        }
-    }
-
-    private fun getCurrentActionStack(): ActionStack? {
-        var actionStack: ActionStack? = null
-
-        sendEvent(Event.MapController.FetchSelected { currentMap ->
-            if (currentMap != null) {
-                actionStack = actionStacks.getOrPut(currentMap) { ActionStack() }
-            }
-        })
-
-        return actionStack
-    }
+    private fun getMapActionStack(map: Dmm): ActionStack = actionStacks.getOrPut(map) { ActionStack() }
 
     private fun updateActionBalance(isPositive: Boolean) {
         sendEvent(Event.MapController.FetchSelected { currentMap ->
@@ -60,12 +29,65 @@ class ActionController : EventConsumer, EventSender {
                 val currentBalance = actionBalanceStorage.getOrPut(currentMap) { 0 }
                 val newBalance = if (isPositive) currentBalance + 1 else currentBalance - 1
                 actionBalanceStorage.put(currentMap, newBalance)
+                notifyActionBalanceChanged(currentMap)
+            }
+        })
+    }
 
-                actionStacks[currentMap]!!.let { (undo, redo) ->
-                    sendEvent(Event.Global.ActionStatusChanged(ActionStatus(undo.isNotEmpty(), redo.isNotEmpty())))
+    private fun notifyActionBalanceChanged(map: Dmm) {
+        getMapActionStack(map).let { (undo, redo) ->
+            sendEvent(Event.Global.ActionStatusChanged(ActionStatus(undo.isNotEmpty(), redo.isNotEmpty())))
+        }
+    }
+
+    private fun handleAddAction(event: Event<Undoable, Unit>) {
+        sendEvent(Event.MapController.FetchSelected { currentMap ->
+            if (currentMap != null) {
+                getMapActionStack(currentMap).let { (undo, redo) ->
+                    redo.clear()
+                    undo.push(event.body)
+                    updateActionBalance(true)
                 }
             }
         })
+    }
+
+    private fun handleUndoAction() {
+        sendEvent(Event.MapController.FetchSelected { currentMap ->
+            if (currentMap != null) {
+                getMapActionStack(currentMap).let { (undo, redo) ->
+                    redo.push(undo.pop().doAction())
+                    updateActionBalance(false)
+                    sendEvent(Event.Global.RefreshFrame())
+                }
+            }
+        })
+    }
+
+    private fun handleRedoAction() {
+        sendEvent(Event.MapController.FetchSelected { currentMap ->
+            if (currentMap != null) {
+                getMapActionStack(currentMap).let { (undo, redo) ->
+                    undo.push(redo.pop().doAction())
+                    updateActionBalance(true)
+                    sendEvent(Event.Global.RefreshFrame())
+                }
+            }
+        })
+    }
+
+    private fun handleResetEnvironment() {
+        actionStacks.clear()
+        actionBalanceStorage.clear()
+    }
+
+    private fun handleSwitchMap(event: Event<Dmm, Unit>) {
+        notifyActionBalanceChanged(event.body)
+    }
+
+    private fun handleCloseMap(event: Event<Dmm, Unit>) {
+        actionStacks.remove(event.body)
+        actionBalanceStorage.remove(event.body)
     }
 
     private data class ActionStack(
