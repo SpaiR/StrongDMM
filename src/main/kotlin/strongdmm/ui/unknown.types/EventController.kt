bd8b11a39821b7d4fcfd9f1807f8af878b5924d0
SpaiR <klimbetoo@gmail.com>
2020-04-22 11:46:40 +0300

    [ui-refactor] UnknownTypesPanel

---
+++ src/main/kotlin/strongdmm/ui/unknown.types/EventController.kt
@@ -0,0 +1,19 @@
+package strongdmm.ui.unknown.types
+
+import strongdmm.byond.dmm.MapPos
+import strongdmm.event.Event
+import strongdmm.event.EventHandler
+import strongdmm.event.type.ui.TriggerUnknownTypesPanelUi
+
+class EventController(
+    private val state: State
+) : EventHandler {
+    init {
+        consumeEvent(TriggerUnknownTypesPanelUi.Open::class.java, ::handleOpen)
+    }
+
+    private fun handleOpen(event: Event<List<Pair<MapPos, String>>, Unit>) {
+        state.unknownTypes = event.body
+        state.isOpened.set(true)
+    }
+}
