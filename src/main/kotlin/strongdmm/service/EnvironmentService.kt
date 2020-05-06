5fe3bbdc4ca3de49e53be5dfd3f183986913d6d3
SpaiR <klimbetoo@gmail.com>
2020-05-07 01:25:05 +0300

    resolve #27, ask about unsaved map changes when switching environment

--- src/main/kotlin/strongdmm/service/EnvironmentService.kt
+++ src/main/kotlin/strongdmm/service/EnvironmentService.kt
@@ -9,6 +9,7 @@ import strongdmm.event.Event
 import strongdmm.event.EventHandler
 import strongdmm.event.type.Reaction
 import strongdmm.event.type.service.TriggerEnvironmentService
+import strongdmm.event.type.service.TriggerMapHolderService
 import java.io.File
 import kotlin.concurrent.thread
 
@@ -20,16 +21,16 @@ class EnvironmentService : Service, EventHandler {
         consumeEvent(TriggerEnvironmentService.FetchOpenedEnvironment::class.java, ::handleFetchOpenedEnvironment)
     }
 
-    private fun handleOpenEnvironment(event: Event<File, Unit>) {
+    private fun openEnvironment(file: File) {
         sendEvent(Reaction.EnvironmentReset())
 
         GlobalDmiHolder.resetEnvironment()
         GlobalTileItemHolder.resetEnvironment()
 
         thread(start = true) {
-            sendEvent(Reaction.EnvironmentLoadStarted(event.body))
+            sendEvent(Reaction.EnvironmentLoadStarted(file))
 
-            environment = SdmmParser().parseDme(event.body)
+            environment = SdmmParser().parseDme(file)
 
             GlobalDmiHolder.environmentRootPath = environment.absRootDirPath
             GlobalTileItemHolder.environment = environment
@@ -38,6 +39,18 @@ class EnvironmentService : Service, EventHandler {
 
             sendEvent(Reaction.EnvironmentChanged(environment))
             sendEvent(Reaction.EnvironmentLoadStopped(true))
+        }
+    }
+
+    private fun handleOpenEnvironment(event: Event<File, Unit>) {
+        if (this::environment.isInitialized) {
+            sendEvent(TriggerMapHolderService.CloseAllMaps {
+                if (it) {
+                    openEnvironment(event.body)
+                }
+            })
+        } else {
+            openEnvironment(event.body)
         }
     }
 
