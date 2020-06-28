63c1c650a7b1013c8ebf921eb22d80f5affb234e
SpaiR <klimbetoo@gmail.com>
2020-06-28 21:46:45 +0300

    Cleanup GLFW backend logic

--- src/main/kotlin/strongdmm/window/AppWindow.kt
+++ src/main/kotlin/strongdmm/window/AppWindow.kt
@@ -3,10 +3,9 @@ package strongdmm.window
 import imgui.ImFontConfig
 import imgui.ImGui
 import imgui.ImGuiFreeType
-import imgui.callback.ImStrConsumer
-import imgui.callback.ImStrSupplier
 import imgui.flag.*
 import imgui.gl3.ImGuiImplGl3
+import imgui.glfw.ImGuiImplGlfw
 import org.lwjgl.BufferUtils
 import org.lwjgl.glfw.Callbacks
 import org.lwjgl.glfw.GLFW.*
@@ -38,32 +37,36 @@ abstract class AppWindow(title: String) {
         }
     }
 
-    // For mouse tracking
-    private val mousePosX: DoubleArray = DoubleArray(1)
-    private val mousePosY: DoubleArray = DoubleArray(1)
-
-    // Mouse cursors provided by GLFW
-    private val mouseCursors: LongArray = LongArray(ImGuiMouseCursor.COUNT)
-
-    private val sync: Sync = Sync()
-    private val imGuiGl3: ImGuiImplGl3 = ImGuiImplGl3()
+    private val sync = Sync()
+    private val imGuiGlfw = ImGuiImplGlfw()
+    private val imGuiGl3 = ImGuiImplGl3()
 
     private lateinit var fontData: ByteArray
     private lateinit var iconData: ByteArray
 
     init {
-        initGlfw(title)
-        initImGui()
+        ImGui.createContext()
+
+        setupGlfw(title)
+        imGuiGlfw.init(Window.ptr, true)
+
+        setupImGui()
+        imGuiGl3.init()
     }
 
     fun start() {
         loop()
-        destroyImGui()
-        destroyGlfw()
+
+        imGuiGl3.dispose()
+        imGuiGlfw.dispose()
+
+        ImGui.destroyContext()
+
+        disposeWindow()
     }
 
     // Method initializes GLFW window
-    private fun initGlfw(title: String) {
+    private fun setupGlfw(title: String) {
         // Setup an error callback. The default implementation will print the error message in System.err.
         GLFWErrorCallback.createPrint(System.err).set()
 
@@ -106,106 +109,8 @@ abstract class AppWindow(title: String) {
     }
 
     // Here we will initialize ImGui stuff.
-    private fun initImGui() {
-        ImGui.createContext()
-        ImGui.styleColorsDark()
-
-        val io = ImGui.getIO()
-
-        io.iniFilename = null
-        io.backendFlags = ImGuiBackendFlags.HasMouseCursors
-
-        // Keyboard mapping. ImGui will use those indices to peek into the io.KeysDown[] array.
-        val keyMap = IntArray(ImGuiKey.COUNT)
-        keyMap[ImGuiKey.Tab] = GLFW_KEY_TAB
-        keyMap[ImGuiKey.LeftArrow] = GLFW_KEY_LEFT
-        keyMap[ImGuiKey.RightArrow] = GLFW_KEY_RIGHT
-        keyMap[ImGuiKey.UpArrow] = GLFW_KEY_UP
-        keyMap[ImGuiKey.DownArrow] = GLFW_KEY_DOWN
-        keyMap[ImGuiKey.PageUp] = GLFW_KEY_PAGE_UP
-        keyMap[ImGuiKey.PageDown] = GLFW_KEY_PAGE_DOWN
-        keyMap[ImGuiKey.Home] = GLFW_KEY_HOME
-        keyMap[ImGuiKey.End] = GLFW_KEY_END
-        keyMap[ImGuiKey.Insert] = GLFW_KEY_INSERT
-        keyMap[ImGuiKey.Delete] = GLFW_KEY_DELETE
-        keyMap[ImGuiKey.Backspace] = GLFW_KEY_BACKSPACE
-        keyMap[ImGuiKey.Space] = GLFW_KEY_SPACE
-        keyMap[ImGuiKey.Enter] = GLFW_KEY_ENTER
-        keyMap[ImGuiKey.Escape] = GLFW_KEY_ESCAPE
-        keyMap[ImGuiKey.KeyPadEnter] = GLFW_KEY_KP_ENTER
-        keyMap[ImGuiKey.A] = GLFW_KEY_A
-        keyMap[ImGuiKey.C] = GLFW_KEY_C
-        keyMap[ImGuiKey.V] = GLFW_KEY_V
-        keyMap[ImGuiKey.X] = GLFW_KEY_X
-        keyMap[ImGuiKey.Y] = GLFW_KEY_Y
-        keyMap[ImGuiKey.Z] = GLFW_KEY_Z
-        io.setKeyMap(keyMap)
-
-        // Mouse cursors mapping
-        mouseCursors[ImGuiMouseCursor.Arrow] = glfwCreateStandardCursor(GLFW_ARROW_CURSOR)
-        mouseCursors[ImGuiMouseCursor.TextInput] = glfwCreateStandardCursor(GLFW_IBEAM_CURSOR)
-        mouseCursors[ImGuiMouseCursor.ResizeAll] = glfwCreateStandardCursor(GLFW_ARROW_CURSOR)
-        mouseCursors[ImGuiMouseCursor.ResizeNS] = glfwCreateStandardCursor(GLFW_VRESIZE_CURSOR)
-        mouseCursors[ImGuiMouseCursor.ResizeEW] = glfwCreateStandardCursor(GLFW_HRESIZE_CURSOR)
-        mouseCursors[ImGuiMouseCursor.ResizeNESW] = glfwCreateStandardCursor(GLFW_ARROW_CURSOR)
-        mouseCursors[ImGuiMouseCursor.ResizeNWSE] = glfwCreateStandardCursor(GLFW_ARROW_CURSOR)
-        mouseCursors[ImGuiMouseCursor.Hand] = glfwCreateStandardCursor(GLFW_HAND_CURSOR)
-        mouseCursors[ImGuiMouseCursor.NotAllowed] = glfwCreateStandardCursor(GLFW_ARROW_CURSOR)
-
-        // Here goes GLFW callbacks to update user input stuff in ImGui
-        glfwSetKeyCallback(Window.ptr) { _, key: Int, _, action: Int, _ ->
-            if (action == GLFW_PRESS) {
-                io.setKeysDown(key, true)
-            } else if (action == GLFW_RELEASE) {
-                io.setKeysDown(key, false)
-            }
-
-            io.keyCtrl = io.getKeysDown(GLFW_KEY_LEFT_CONTROL) || io.getKeysDown(GLFW_KEY_RIGHT_CONTROL)
-            io.keyShift = io.getKeysDown(GLFW_KEY_LEFT_SHIFT) || io.getKeysDown(GLFW_KEY_RIGHT_SHIFT)
-            io.keyAlt = io.getKeysDown(GLFW_KEY_LEFT_ALT) || io.getKeysDown(GLFW_KEY_RIGHT_ALT)
-            io.keySuper = io.getKeysDown(GLFW_KEY_LEFT_SUPER) || io.getKeysDown(GLFW_KEY_RIGHT_SUPER)
-        }
-
-        glfwSetCharCallback(Window.ptr) { _, c: Int ->
-            if (c != GLFW_KEY_DELETE) {
-                io.addInputCharacter(c)
-            }
-        }
-
-        glfwSetMouseButtonCallback(Window.ptr) { _, button: Int, action: Int, _ ->
-            val mouseDown = BooleanArray(5)
-
-            mouseDown[0] = button == GLFW_MOUSE_BUTTON_1 && action != GLFW_RELEASE
-            mouseDown[1] = button == GLFW_MOUSE_BUTTON_2 && action != GLFW_RELEASE
-            mouseDown[2] = button == GLFW_MOUSE_BUTTON_3 && action != GLFW_RELEASE
-            mouseDown[3] = button == GLFW_MOUSE_BUTTON_4 && action != GLFW_RELEASE
-            mouseDown[4] = button == GLFW_MOUSE_BUTTON_5 && action != GLFW_RELEASE
-
-            io.setMouseDown(mouseDown)
-
-            if (!io.wantCaptureMouse && mouseDown[1]) {
-                ImGui.setWindowFocus(null)
-            }
-        }
-
-        glfwSetScrollCallback(Window.ptr) { _, xOffset: Double, yOffset: Double ->
-            io.mouseWheelH = io.mouseWheelH + xOffset.toFloat()
-            io.mouseWheel = io.mouseWheel + yOffset.toFloat()
-        }
-
-        io.setSetClipboardTextFn(object : ImStrConsumer() {
-            override fun accept(s: String) {
-                glfwSetClipboardString(Window.ptr, s)
-            }
-        })
-
-        io.setGetClipboardTextFn(object : ImStrSupplier() {
-            override fun get(): String? {
-                return glfwGetClipboardString(Window.ptr).let { it ?: "" }
-            }
-        })
-
-        // Fonts configuration
+    private fun setupImGui() {
+        ImGui.getIO().iniFilename = null
 
         // Read font
         javaClass.classLoader.getResourceAsStream("fonts/Ruda-Bold.ttf")!!.use {
@@ -218,26 +123,16 @@ abstract class AppWindow(title: String) {
         }
 
         configureFonts()
-
-        imGuiGl3.init()
     }
 
     private fun loop() {
-        var time = 0.0 // to track our frame delta value
-
         // Respect alpha channel
         glEnable(GL_BLEND)
         glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
 
         // Run the rendering loop until the user has attempted to close the window
         while (Window.isRunning) { // Count frame delta value
-            val currentTime = glfwGetTime()
-            val deltaTime = if (time > 0) currentTime - time else (1f / 60f).toDouble()
-            time = currentTime
-
-            updateWindowProperties()
-            updateImGuiIO(deltaTime.toFloat())
-            updateMouseCursor()
+            glfwGetWindowSize(Window.ptr, Window._width, Window._height)
 
             startFrame()
             appLoop()
@@ -248,35 +143,12 @@ abstract class AppWindow(title: String) {
         }
     }
 
-    private fun updateWindowProperties() {
-        glfwGetWindowSize(Window.ptr, Window._width, Window._height)
-        glfwGetFramebufferSize(Window.ptr, Window._fbWidth, Window._fbHeight)
-        glfwGetCursorPos(Window.ptr, mousePosX, mousePosY)
-    }
-
-    private fun updateImGuiIO(deltaTime: Float) {
-        val io = ImGui.getIO()
-
-        val scaleX = Window._fbWidth[0].toFloat() / Window._width[0]
-        val scaleY = Window._fbHeight[0].toFloat() / Window._height[0]
-
-        io.setDisplaySize(Window._fbWidth[0].toFloat(), Window._fbHeight[0].toFloat())
-        io.setDisplayFramebufferScale(scaleX, scaleY)
-        io.setMousePos(mousePosX[0].toFloat() * scaleX, mousePosY[0].toFloat() * scaleY)
-
-        io.deltaTime = deltaTime
-    }
-
-    private fun updateMouseCursor() {
-        val imguiCursor = ImGui.getMouseCursor()
-        glfwSetCursor(Window.ptr, mouseCursors[imguiCursor])
-        glfwSetInputMode(Window.ptr, GLFW_CURSOR, GLFW_CURSOR_NORMAL)
-    }
-
     private fun startFrame() {
+        imGuiGlfw.newFrame()
+        ImGui.newFrame()
+
         glClearColor(.25f, .25f, .5f, 1f)
         glClear(GL_COLOR_BUFFER_BIT)
-        ImGui.newFrame()
     }
 
     private fun endFrame() {
@@ -371,16 +243,7 @@ abstract class AppWindow(title: String) {
         }
     }
 
-    // If you want to clean a room after yourself - do it by yourself
-    private fun destroyImGui() {
-        imGuiGl3.dispose()
-        ImGui.destroyContext()
-    }
-
-    private fun destroyGlfw() {
-        for (mouseCursor in mouseCursors) {
-            glfwDestroyCursor(mouseCursor)
-        }
+    private fun disposeWindow() {
         Callbacks.glfwFreeCallbacks(Window.ptr)
         glfwDestroyWindow(Window.ptr)
         glfwTerminate()
