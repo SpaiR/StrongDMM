7279fb7025be3624d936a6dad1ba4faa1c498225
SpaiR <klimbetoo@gmail.com>
2021-10-19 11:23:38 +0300

    Fix name variables from the path

--- src/dmapi/dmenv/dme.go
+++ src/dmapi/dmenv/dme.go
@@ -73,7 +73,7 @@ func traverseTree0(root *sdmmparser.ObjectTreeType, parentName string, dme *Dme)
 	}
 
 	if _, ok := variables.Value("name"); !ok {
-		variables.Put("name", "\""+nameFromPath(root.Path, parentName)+"\"")
+		variables.Put("name", nameFromPath(root.Path, parentName))
 	}
 
 	var children []string
