5dce5cec4e3b08c88cbe13860cccdcc311deba21
SpaiR <klimbetoo@gmail.com>
2022-02-18 22:20:45 +0200

    Add version/revision injection

--- src/env/env.go
+++ src/env/env.go
@@ -2,7 +2,7 @@ package env
 
 var (
 	Title    = "StrongDMM"
-	Version  = "v2.0.0.alpha"
+	Version  = "undefined"
 	Revision = "undefined"
 	GitHub   = "https://github.com/SpaiR/StrongDMM"
 	Manifest = "https://spair.github.io/StrongDMM/manifest.json"
