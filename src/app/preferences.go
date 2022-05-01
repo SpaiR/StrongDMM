358c7150abe03c13422bf27658b1f17d24c61aca
SpaiR <klimbetoo@gmail.com>
2022-05-01 12:36:55 +0300

    Refactor preferences configuration

--- src/app/preferences.go
+++ src/app/preferences.go
@@ -1,7 +1,6 @@
 package app
 
 import (
-	"log"
 	"math"
 	"sdmm/app/prefs"
 	"sdmm/app/ui/cpwsarea/wsprefs"
@@ -10,186 +9,90 @@ import (
 
 func (a *app) makePreferences() wsprefs.Prefs {
 	p := wsprefs.MakePrefs()
-	p.Add(wsprefs.GPEditor, a.makePreferenceEditorFormat())
-	p.Add(wsprefs.GPEditor, a.makePreferenceEditorSanitizeVariables())
-	p.Add(wsprefs.GPEditor, a.makePreferenceEditorNudgeMode())
-	p.Add(wsprefs.GPControls, a.makePreferenceControlsAltScrollBehaviour())
-	p.Add(wsprefs.GPControls, a.makePreferenceControlsQuickEditContextMenu())
-	p.Add(wsprefs.GPControls, a.makePreferenceControlsQuickEditMapPane())
-	p.Add(wsprefs.GPInterface, a.makePreferenceInterfaceScale())
-	p.Add(wsprefs.GPInterface, a.makePreferenceInterfaceFps())
-	p.Add(wsprefs.GPApplication, a.makePreferenceApplicationCheckForUpdates())
-	return p
-}
 
-func (a *app) makePreferenceInterfaceFps() wsprefs.IntPref {
-	p := wsprefs.MakeIntPref()
-	p.Name = "Fps"
-	p.Desc = "Controls the application framerate."
-	p.Label = "##fps"
-	p.Min = 30
-	p.Max = math.MaxInt
+	var preferencesPrefabs = map[wsprefs.PrefGroup][]prefPrefab{
+		wsprefs.GPEditor: {
+			optionPrefPrefab{
+				name:    "Save Format",
+				desc:    "Controls the format used by the editor to save the map.",
+				label:   "##save_format",
+				value:   &a.preferencesConfig().Prefs.Editor.SaveFormat,
+				options: prefs.SaveFormats,
+				help:    prefs.SaveFormatHelp,
+			},
+			boolPrefPrefab{
+				name:  "Sanitize Variables",
+				desc:  "Enables sanitizing for variables which are declared on the map, but has the same value as initial.",
+				label: "##sanitize_variables",
+				value: &a.preferencesConfig().Prefs.Editor.SanitizeVariables,
+			},
+			optionPrefPrefab{
+				name:    "Nudge Mode",
+				desc:    "Controls which variables will be changed during the nudge.",
+				label:   "##nudge_mode",
+				value:   &a.preferencesConfig().Prefs.Editor.NudgeMode,
+				options: prefs.SaveNudgeModes,
+			},
+		},
 
-	p.FGet = func() int {
-		return a.preferencesConfig().Prefs.Interface.Fps
-	}
-	p.FSet = func(value int) {
-		log.Println("[app] preferences changing, [interface#fps] to:", value)
-		cfg := a.preferencesConfig()
-		cfg.Prefs.Interface.Fps = value
-		window.SetFps(value)
-	}
+		wsprefs.GPControls: {
+			boolPrefPrefab{
+				name:  "Alternative Scroll Behavior",
+				desc:  "When enabled, scrolling will do panning. Zoom will be available if a Space key pressed.",
+				label: "##alternative_scroll_behavior",
+				value: &a.preferencesConfig().Prefs.Controls.AltScrollBehaviour,
+			},
+			boolPrefPrefab{
+				name:  "Quick Edit: Tile Context Menu",
+				desc:  "Controls whether Quick Edit should be shown in the tile context menu.",
+				label: "##quick_edit:tile_context_menu",
+				value: &a.preferencesConfig().Prefs.Controls.QuickEditContextMenu,
+			},
+			boolPrefPrefab{
+				name:  "Quick Edit: Map Pane",
+				desc:  "Controls whether Quick Edit should be shown on the map pane.",
+				label: "##quick_edit:map_pane",
+				value: &a.preferencesConfig().Prefs.Controls.QuickEditMapPane,
+			},
+		},
 
-	return p
-}
+		wsprefs.GPInterface: {
+			intPrefPrefab{
+				name:  "Scale",
+				desc:  "Controls the interface scale.",
+				label: "%##scale",
+				min:   50,
+				max:   250,
+				value: &a.preferencesConfig().Prefs.Interface.Scale,
+				post: func(int) {
+					a.tmpUpdateScale = true
+				},
+			},
+			intPrefPrefab{
+				name:  "Fps",
+				desc:  "Controls the application framerate.",
+				label: "##fps",
+				min:   30,
+				max:   math.MaxInt,
+				value: &a.preferencesConfig().Prefs.Interface.Fps,
+				post:  window.SetFps,
+			},
+		},
 
-func (a *app) makePreferenceInterfaceScale() wsprefs.IntPref {
-	p := wsprefs.MakeIntPref()
-	p.Name = "Scale"
-	p.Desc = "Controls the interface scale."
-	p.Label = "%##scale"
-	p.Min = 50
-	p.Max = 250
-
-	p.FGet = func() int {
-		return a.preferencesConfig().Prefs.Interface.Scale
-	}
-	p.FSet = func(value int) {
-		log.Println("[app] preferences changing, [interface#scale] to:", value)
-		cfg := a.preferencesConfig()
-		cfg.Prefs.Interface.Scale = value
-		a.tmpUpdateScale = true
-	}
-
-	return p
-}
-
-func (a *app) makePreferenceControlsAltScrollBehaviour() wsprefs.BoolPref {
-	p := wsprefs.MakeBoolPref()
-	p.Name = "Alternative Scroll Behavior"
-	p.Desc = "When enabled, scrolling will do panning. Zoom will be available if a Space key pressed."
-	p.Label = "##alternative_scroll_behavior"
-
-	p.FGet = func() bool {
-		return a.preferencesConfig().Prefs.Controls.AltScrollBehaviour
-	}
-	p.FSet = func(value bool) {
-		log.Println("[app] preferences changing, [controls#alternative_scroll_behaviour] to:", value)
-		cfg := a.preferencesConfig()
-		cfg.Prefs.Controls.AltScrollBehaviour = value
-	}
-
-	return p
-}
-
-func (a *app) makePreferenceControlsQuickEditContextMenu() wsprefs.BoolPref {
-	p := wsprefs.MakeBoolPref()
-	p.Name = "Quick Edit: Tile Context Menu"
-	p.Desc = "Controls whether Quick Edit should be shown in the tile context menu."
-	p.Label = "##quick_edit:tile_context_menu"
-
-	p.FGet = func() bool {
-		return a.preferencesConfig().Prefs.Controls.QuickEditContextMenu
-	}
-	p.FSet = func(value bool) {
-		log.Println("[app] preferences changing, [controls#quick_edit:tile_context_menu] to:", value)
-		cfg := a.preferencesConfig()
-		cfg.Prefs.Controls.QuickEditContextMenu = value
+		wsprefs.GPApplication: {
+			boolPrefPrefab{
+				name:  "Check for Updates",
+				desc:  "When enabled, the editor will always check for updates on startup.",
+				label: "##check_for_updates",
+				value: &a.preferencesConfig().Prefs.Application.CheckForUpdates,
+			},
+		},
 	}
 
-	return p
-}
-
-func (a *app) makePreferenceControlsQuickEditMapPane() wsprefs.BoolPref {
-	p := wsprefs.MakeBoolPref()
-	p.Name = "Quick Edit: Map Pane"
-	p.Desc = "Controls whether Quick Edit should be shown on the map pane."
-	p.Label = "##quick_edit:map_pane"
-
-	p.FGet = func() bool {
-		return a.preferencesConfig().Prefs.Controls.QuickEditMapPane
-	}
-	p.FSet = func(value bool) {
-		log.Println("[app] preferences changing, [controls#quick_edit:map_pane] to:", value)
-		cfg := a.preferencesConfig()
-		cfg.Prefs.Controls.QuickEditMapPane = value
-	}
-
-	return p
-}
-
-func (a *app) makePreferenceEditorFormat() wsprefs.OptionPref {
-	p := wsprefs.MakeOptionPref()
-	p.Name = "Save Format"
-	p.Desc = "Controls the format used by the editor to save the map."
-	p.Label = "##save_format"
-
-	p.FGet = func() string {
-		return a.preferencesConfig().Prefs.Editor.SaveFormat
-	}
-	p.FSet = func(value string) {
-		log.Println("[app] preferences changing, [editor#save_format] to:", value)
-		cfg := a.preferencesConfig()
-		cfg.Prefs.Editor.SaveFormat = value
-	}
-
-	p.Options = prefs.SaveFormats
-	p.Help = prefs.SaveFormatHelp
-
-	return p
-}
-
-func (a *app) makePreferenceEditorSanitizeVariables() wsprefs.BoolPref {
-	p := wsprefs.MakeBoolPref()
-	p.Name = "Sanitize Variables"
-	p.Desc = "Enables sanitizing for variables which are declared on the map, but has the same value as initial."
-	p.Label = "##sanitize_variables"
-
-	p.FGet = func() bool {
-		return a.preferencesConfig().Prefs.Editor.SanitizeVariables
-	}
-	p.FSet = func(value bool) {
-		log.Println("[app] preferences changing, [editor#sanitize_variables] to:", value)
-		cfg := a.preferencesConfig()
-		cfg.Prefs.Editor.SanitizeVariables = value
-	}
-
-	return p
-}
-
-func (a *app) makePreferenceEditorNudgeMode() wsprefs.OptionPref {
-	p := wsprefs.MakeOptionPref()
-	p.Name = "Nudge Mode"
-	p.Desc = "Controls which variables will be changed during the nudge."
-	p.Label = "##nudge_mode"
-
-	p.FGet = func() string {
-		return a.preferencesConfig().Prefs.Editor.NudgeMode
-	}
-	p.FSet = func(value string) {
-		log.Println("[app] preferences changing, [editor#nudge_mode] to:", value)
-		cfg := a.preferencesConfig()
-		cfg.Prefs.Editor.NudgeMode = value
-	}
-
-	p.Options = prefs.SaveNudgeModes
-
-	return p
-}
-
-func (a *app) makePreferenceApplicationCheckForUpdates() wsprefs.BoolPref {
-	p := wsprefs.MakeBoolPref()
-	p.Name = "Check for Updates"
-	p.Desc = "When enabled, the editor will always check for updates on startup."
-	p.Label = "##check_for_updates"
-
-	p.FGet = func() bool {
-		return a.preferencesConfig().Prefs.Application.CheckForUpdates
-	}
-	p.FSet = func(value bool) {
-		log.Println("[app] preferences changing, [application#check_for_updates] to:", value)
-		cfg := a.preferencesConfig()
-		cfg.Prefs.Application.CheckForUpdates = value
+	for group, prefabs := range preferencesPrefabs {
+		for _, prefab := range prefabs {
+			p.Add(group, prefab.make())
+		}
 	}
 
 	return p
