8543f0567f65a3eefefad8c855a127bff3696fb9
Tad Hardesty <tad@platymuus.com>
2025-12-27 01:33:02 -0800

    Show var modifications under prefab name

--- internal/app/ui/cpprefabs/process.go
+++ internal/app/ui/cpprefabs/process.go
@@ -2,7 +2,9 @@ package cpprefabs
 
 import (
 	"fmt"
+	"strings"
 
+	"sdmm/internal/dmapi/dmvars"
 	w "sdmm/internal/imguiext/widget"
 
 	"github.com/SpaiR/imgui-go"
@@ -56,11 +58,34 @@ func (p *Prefabs) Process(int32) {
 		imgui.SameLine()
 
 		imgui.BeginGroup()
+		imgui.PushStyleVarVec2(imgui.StyleVarItemSpacing, imgui.Vec2{X: 0, Y: 0})
 		imgui.Text(node.name)
+		imgui.TextColored(imgui.Vec4{X: 0.6, Y: 0.6, Z: 0.6, W: 1}, describeVars(node.orig.Vars()))
+		imgui.PopStyleVar()
 		imgui.EndGroup()
 
 		imgui.EndGroup()
 
 		node.visHeight = imgui.ItemRectMax().Y - imgui.ItemRectMin().Y
 	}
+}
+
+func describeVars(variables *dmvars.Variables) string {
+	// "name" is already plainly visible and can be skipped.
+	// "icon", "icon_state", "dir", and "color" do affect the sprite, but are
+	// included in the list in case the effect is not obvious.
+	b := strings.Builder{}
+	for _, key := range variables.Iterate() {
+		if key == "name" {
+			continue
+		}
+		if b.Len() > 0 {
+			b.WriteString("; ")
+		}
+		b.WriteString(key)
+		b.WriteString(" = ")
+		value, _ := variables.Value(key)
+		b.WriteString(value)
+	}
+	return b.String()
 }
